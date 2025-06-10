9fed0fd8e816df85d7f99377ebabd689dc51eaff
Lim Vithyea <cheakh212@gmail.com>
2025-06-10 13:41:47 +0700

    final fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash; 
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +16,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+         DB::table('users')->insert([
+            'name' => 'Seng',
+            'email' => 'seng@example.com',
+            'password' => Hash::make('12345678'), // Always hash passwords!
+            'role' => '1',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 }
