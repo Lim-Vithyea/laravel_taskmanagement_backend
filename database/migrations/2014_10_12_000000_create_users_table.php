76249ceaba1a01af542a84ed9330f2dfb80dc823
Lim Vithyea <cheakh212@gmail.com>
2025-06-14 10:44:24 +0700

    add default user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash; 
 
 
 class CreateUsersTable extends Migration
@@ -26,6 +28,14 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
+        DB::table('users')->insert([
+            'name' => 'Seng',
+            'email' => 'seng@example.com',
+            'password' => Hash::make('12345678'), // Always hash passwords!
+            'role' => '1',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 
     /**
